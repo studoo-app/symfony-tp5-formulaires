89c5e891bc492f988c38bfb0873d4993eabf29bc
JPechberty <j.pechberty@gmail.com>
2025-11-03 12:07:44 +0100

    Etape 2.1

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +19,45 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-            ->add('dateNaissance')
-            ->add('entreprise')
-            ->add('poste')
-            ->add('bio')
-            ->add('dateCreation')
-            ->add('estActif')
+            ->add('email',EmailType::class, [
+                'label' => 'Adresse e-mail',
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('telephone', TelType::class::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+            ])
+            ->add('dateNaissance', DateTimeType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('entreprise', TextType::class, [
+                'label' => 'Entreprise',
+                'required' => false,
+            ])
+            ->add('poste', TextType::class, [
+                'label' => 'Poste',
+                'required' => false,
+            ])
+            ->add('bio', TextareaType::class, [
+                'label' => 'Biographie',
+                'required' => false,
+            ])
         ;
+
+        if($options['data']->getId() === null) {
+            $builder->add('password',RepeatedType::class, [
+                'type' =>  PasswordType::class,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez le mot de passe'],
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
